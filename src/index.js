ae0aab4f852f7a9b8af25372864cfc6c9e96cf60
Vyacheslav <ermak519@gmail.com>
2022-02-17 20:09:36 +0300

    prod

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './store/reducers';
@@ -8,7 +8,7 @@ import { App } from './components/App';
 
 const store = createStore(
   combineReducers(rootReducer()),
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
